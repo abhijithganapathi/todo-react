e02a6b8d713ade4d50cf3d525f5e85c854a94400
abhijithganapathi <abhijith.ganapathi@gmail.com>
2023-11-14 20:02:51 +0530

    debugging deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createHashRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import ToDoApp from './Components/ToDoApp.jsx'
 import Header from './Components/Header.jsx'
 import About from './Components/About.jsx'
 import ErrorPage from './Components/ErrorPage.jsx'
 
-const router=createHashRouter([
+const router=createBrowserRouter([
   {
     path:'/todo-react/',
     element:<ToDoApp/>,
