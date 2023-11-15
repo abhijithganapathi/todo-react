f64a86a5a9e44361d840aed4a469f730e82f4469
abhijithganapathi <abhijith.ganapathi@gmail.com>
2023-11-15 21:51:20 +0530

    debugging deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import ToDoApp from './Components/ToDoApp';
+import About from './Components/About';
+import Header from './Components/Header';
+import { Outlet } from "react-router-dom";
+import ErrorPage from './Components/ErrorPage';
+
+export default function Layout() {
+  return (
+      <>
+        <Header />
+        <Outlet />
+      </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout/>,
+    errorElement: <ErrorPage />,
+    children: [  
+      {
+        path: '/todo-react/',
+        element: <ToDoApp />
+      },
+      {
+        path: '/todo-react/about',
+        element: <About />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
